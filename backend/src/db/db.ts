dcf7b3afea397b719e2d591ea1fbc9b52660e8b0
Siedemus <barsak1177@gmail.com>
2024-07-29 17:09:47 +0200

    delete unnecessary files

--- backend/src/db/db.ts
+++ backend/src/db/db.ts
@@ -1,7 +1,5 @@
 import { Pool } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-serverless";
-import { messages, rooms, userRoomSubscriptions, users } from "./schema";
-import { eq } from "drizzle-orm";
 
 const pool = new Pool({ connectionString: process.env.DB_URL });
 const db = drizzle(pool);
