590a151fad7cef6718006c446071514e88c85158
Siedemus <barsak1177@gmail.com>
2024-08-16 14:40:35 +0200

    add roomQuery handling and prevent img room tags from changing color on rerender

--- frontend/src/components/Rooms.tsx
+++ frontend/src/components/Rooms.tsx
@@ -2,13 +2,15 @@ import { Room } from "../types/types";
 import hashIcon from "../resources/images/hash.svg";
 import roomColors from "../resources/roomColors";
 import { useSearchParams } from "react-router-dom";
+import useQueryParam from "../hooks/useQueryParam";
 
 const Rooms = ({ rooms }: { rooms: Room[] }) => {
+  const [, updateRoomQuery] = useQueryParam("r");
   const [searchParams] = useSearchParams();
-  const query = searchParams.get("q");
+  const searchQuery = searchParams.get("q");
 
-  const filteredRooms = query
-    ? rooms.filter((user) => user.name.includes(query))
+  const filteredRooms = searchQuery
+    ? rooms.filter((user) => user.name.includes(searchQuery))
     : rooms;
 
   return (
@@ -16,18 +18,20 @@ const Rooms = ({ rooms }: { rooms: Room[] }) => {
       <h2 className="py-2 font-bold">Rooms</h2>
       {filteredRooms.length > 0 ? (
         <ul className="grid grid-cols-2 p-2 gap-4 max-h-[40vh] overflow-y-auto">
-          {filteredRooms.map((user) => {
-            const randomNumber = Math.floor(Math.random() * roomColors.length);
-            const color = roomColors[randomNumber];
-            console.log(color);
+          {filteredRooms.map((room, i) => {
+            const color = roomColors[i % roomColors.length];
 
             return (
-              <li className="flex items-center gap-2" key={user.id}>
+              <li
+                className="flex items-center gap-2 hover:bg-periwinkleGray rounded-full duration-300 cursor-pointer hover:text-aliceBlue"
+                key={room.id}
+                onClick={() => updateRoomQuery(room.id.toString())}
+              >
                 <img
                   src={hashIcon}
                   className={`w-11 h-11 rounded-full bg-${color}`}
                 ></img>
-                <p className="truncate">{user.name}</p>
+                <p className="truncate">{room.name}</p>
               </li>
             );
           })}
