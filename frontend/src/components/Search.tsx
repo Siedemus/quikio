8083c18c928d2e76219def0002daef6cbc1cce48
Siedemus <barsak1177@gmail.com>
2024-08-16 14:19:40 +0200

    delete old logic and use useQueryParam

--- frontend/src/components/Search.tsx
+++ frontend/src/components/Search.tsx
@@ -1,19 +1,7 @@
-import React, { useState } from "react";
+import useQueryParam from "../hooks/useQueryParam";
 
 const Search = () => {
-  const searchParams = new URLSearchParams(window.location.search);
-  const initialQuery = searchParams.get("q") ? searchParams.get("q") : "";
-  const [query, setQuery] = useState(initialQuery);
-
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const newQuery = e.target.value;
-
-    setQuery(newQuery);
-
-    searchParams.set("q", newQuery);
-    const newUrl = `${window.location.pathname}?${searchParams.toString()}`;
-    window.history.pushState({}, "", newUrl);
-  };
+  const [query, updateQuery] = useQueryParam("q");
 
   return (
     <section className="max-w-[350px] p-4 flex justify-center items-center border-r border-b border-periwinkleGray min-h-[8vh]">
@@ -21,7 +9,7 @@ const Search = () => {
         className="placeholder:text-gray-500 bg-aliceBlue pl-4 py-2 w-full h-11 border border-hippieBlue rounded-2xl"
         placeholder="Search users or channels..."
         value={query ? query : ""}
-        onChange={handleInputChange}
+        onChange={(e) => updateQuery(e.target.value)}
       />
     </section>
   );
