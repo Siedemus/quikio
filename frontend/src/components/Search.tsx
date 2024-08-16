885c26f0d2c195ffd6da9c51db9ed11890c8118a
Siedemus <barsak1177@gmail.com>
2024-08-16 09:09:39 +0200

    add min height

--- frontend/src/components/Search.tsx
+++ frontend/src/components/Search.tsx
@@ -16,7 +16,7 @@ const Search = () => {
   };
 
   return (
-    <section className="max-w-[350px] p-4 flex justify-center items-center border-r border-b border-periwinkleGray">
+    <section className="max-w-[350px] p-4 flex justify-center items-center border-r border-b border-periwinkleGray min-h-[8vh]">
       <input
         className="placeholder:text-gray-500 bg-aliceBlue pl-4 py-2 w-full h-11 border border-hippieBlue rounded-2xl"
         placeholder="Search users or channels..."
