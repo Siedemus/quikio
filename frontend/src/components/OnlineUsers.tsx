0f0c71ab78372f5f1206cb91369a24957f082c16
Siedemus <barsak1177@gmail.com>
2024-08-18 01:48:02 +0200

    add hover effect in OnlineUsers component

--- frontend/src/components/OnlineUsers.tsx
+++ frontend/src/components/OnlineUsers.tsx
@@ -14,9 +14,12 @@ const OnlineUsers = ({ onlineUsers }: { onlineUsers: OnlineUser[] }) => {
     <section className="max-w-[350px] min-h-[46vh] px-4 pt-2 border-r border-b border-periwinkleGray">
       <h2 className="py-2 font-bold">Online users</h2>
       {filteredOnlineUsers.length > 0 ? (
-        <ul className="grid grid-cols-2 p-2 gap-4 max-h-[40vh] overflow-y-auto scroll-smooth">
+        <ul className="grid grid-cols-2 p-2 gap-4 max-h-[40vh] overflow-y-auto scroll-smooth ">
           {filteredOnlineUsers.map((user) => (
-            <li className="flex items-center gap-2" key={user.id}>
+            <li
+              className="flex items-center gap-2 hover:bg-solitude rounded-full duration-300 cursor-pointer hover:text-hippieBlue"
+              key={user.id}
+            >
               <img
                 src={placeholderUser}
                 className="w-11 h-11 rounded-full"
