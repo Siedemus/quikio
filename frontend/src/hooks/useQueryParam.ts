784298d32d26ca49f7db68cf3204165a2cc478c8
Siedemus <barsak1177@gmail.com>
2024-08-26 15:03:26 +0200

    improve useQueryParam to handle read search params

--- frontend/src/hooks/useQueryParam.ts
+++ frontend/src/hooks/useQueryParam.ts
@@ -1,20 +1,29 @@
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 
 const useQueryParam = (key: string) => {
   const [searchParams, setSearchParams] = useSearchParams();
   const [query, setQuery] = useState(() => searchParams.get(key) || "");
 
+  useEffect(() => {
+    const newQuery = searchParams.get(key) || "";
+    if (newQuery !== query) {
+      updateQuery(newQuery);
+    }
+  }, [searchParams, key]);
+
   const updateQuery = useCallback(
     (value: string) => {
       const newSearchParams = new URLSearchParams(searchParams.toString());
-      if (value) {
-        newSearchParams.set(key, value);
-        setQuery(value);
-      } else {
+      if (!value) {
         newSearchParams.delete(key);
+        setSearchParams(newSearchParams);
         setQuery("");
+        return;
       }
+
+      newSearchParams.set(key, value);
+      setQuery(value);
       setSearchParams(newSearchParams);
     },
     [searchParams, key]
