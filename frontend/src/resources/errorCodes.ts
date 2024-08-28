c5eb5d4db92b1d8d31b5391717fbc99e3407eefe
Siedemus <barsak1177@gmail.com>
2024-08-28 10:52:18 +0200

    add new error code

--- frontend/src/resources/errorCodes.ts
+++ frontend/src/resources/errorCodes.ts
@@ -1,22 +1,22 @@
 const errorCodes = {
-  UNEXPECTED_ERROR_DURING_CLOSING_CONNECTION: {
+  UNEXPECTED_ERROR_TRYING_AGAIN: {
     code: 1,
     content:
-      "An unexpected error occurred while closing the connection and removing listeners. Please refresh the page and try again.",
-  },
-  UNEXPECTED_ERROR: {
-    code: 2,
-    content: "An unexpected error occurred. Please try again later.",
+      "An unexpected error occurred. We are trying to reconnect to the server.",
   },
   NOT_HANDLED_ERROR: {
-    code: 3,
-    content:
-      "An unhandled event was encountered.",
+    code: 2,
+    content: "An unhandled event was encountered.",
   },
   CANT_CONNECT_TO_SERVER: {
-    code: 4,
+    code: 3,
     content:
       "Unable to establish a connection with the server. Please check your internet connection and try again later.",
+  },
+  WEBSOCKET_REF_NULL: {
+    code: 4,
+    content:
+      "WebSocket reference is null. Please ensure the WebSocket is properly initialized and try again.",
   },
 };
 
