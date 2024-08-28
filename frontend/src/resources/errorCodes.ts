63029e3fb20fa1e5f4f523ebad321ef980bcb06a
Siedemus <barsak1177@gmail.com>
2024-08-28 13:47:53 +0200

    change client error codes

--- frontend/src/resources/errorCodes.ts
+++ frontend/src/resources/errorCodes.ts
@@ -1,20 +1,20 @@
 const errorCodes = {
   UNEXPECTED_ERROR_TRYING_AGAIN: {
-    code: 1,
+    code: 201,
     content:
       "An unexpected error occurred. We are trying to reconnect to the server.",
   },
   NOT_HANDLED_ERROR: {
-    code: 2,
+    code: 202,
     content: "An unhandled event was encountered.",
   },
   CANT_CONNECT_TO_SERVER: {
-    code: 3,
+    code: 203,
     content:
       "Unable to establish a connection with the server. Please check your internet connection and try again later.",
   },
   WEBSOCKET_REF_NULL: {
-    code: 4,
+    code: 204,
     content:
       "WebSocket reference is null. Please ensure the WebSocket is properly initialized and try again.",
   },
