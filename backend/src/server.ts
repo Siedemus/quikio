50113cd37dcb76e90d586a1ca69e11ad5a655f37
Siedemus <barsak1177@gmail.com>
2024-08-31 23:14:46 +0200

    prepare server for deploy on render

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,5 @@
-import * as https from "https";
+import * as http from "http";
 import * as ws from "ws";
-import * as fs from "fs";
-import * as path from "path";
-import { fileURLToPath } from "bun";
 import { handleMessage } from "./handlers/handleMessage";
 import { JSONToMessage } from "./utils/JSONToMessage";
 import { sendErrorMessage } from "./utils/sendErrorMessage";
@@ -11,39 +8,25 @@ import onlineUsersManager from "./models/onlineUsersManager";
 const PORT = Number(process.env.PORT) || 8080;
 const HOST = process.env.HOST || "0.0.0.0";
 
-const __dirname = fileURLToPath(path.dirname(import.meta.url));
+const server = http.createServer();
+const wss = new ws.WebSocketServer({ server });
 
-const serverOptions = {
-  cert: fs.readFileSync(path.join(__dirname, "../../certs/server.crt")),
-  key: fs.readFileSync(path.join(__dirname, "../../certs/key.pem")),
-};
+wss.on("connection", (ws) => {
+  ws.on("message", (data) => {
+    try {
+      const message = JSONToMessage(data.toString());
+      handleMessage(message, ws);
+    } catch {
+      sendErrorMessage(ws, "PARSING_ERROR");
+    }
+  });
 
-const server = https.createServer(serverOptions);
-const wss = new ws.WebSocketServer({ noServer: true });
+  ws.on("error", () => {
+    onlineUsersManager.removeUser(ws);
+  });
 
-server.on("upgrade", (req, socket, head) => {
-  if (req.headers.upgrade !== "websocket") {
-    socket.write("HTTP/1.1 400 Bad Request\r\n\r\n"), socket.destroy();
-    return;
-  }
-
-  wss.handleUpgrade(req, socket, head, (ws) => {
-    ws.on("message", (data) => {
-      try {
-        const message = JSONToMessage(data.toString());
-        handleMessage(message, ws);
-      } catch {
-        sendErrorMessage(ws, "PARSING_ERROR");
-      }
-    });
-
-    ws.on("error", () => {
-      onlineUsersManager.removeUser(ws);
-    });
-
-    ws.on("close", () => {
-      onlineUsersManager.removeUser(ws);
-    });
+  ws.on("close", () => {
+    onlineUsersManager.removeUser(ws);
   });
 });
 
